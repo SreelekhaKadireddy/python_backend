47d10cc823bb5135f3bd04c05c91db89849ab0d7
SreelekhaKadireddy <ksreelekha049@gmail.com>
2023-08-09 22:35:06 +0530

    string methods

---
+++ string_methods.py
@@ -0,0 +1,15 @@
+#string methods
+a="Hello , Good morning"
+print(a.capitalize())
+print(a.casefold())
+print(a.center(2))
+print(a.count("o"))
+print(a.encode())
+print(a.endswith("g"))
+print(a.find("o"))
+print(a.expandtabs())
+print(a.index("G"))
+print(a.isalpha())
+print(a.join("!"))
+print(a.replace("G","g"))
+
